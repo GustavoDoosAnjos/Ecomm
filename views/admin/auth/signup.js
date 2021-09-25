26813fe23f3010bbf8c45057cdfc2bf6a5b3506d
Gustavo Oliveira dos Anjos <anjos.gu@gmail.com>
2021-09-25 11:52:35 -0300

    adiciona validação de informações

--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,12 +1,16 @@
-const layout = require('../layout')
+const layout = require('../layout');
+const { getError } = require('../../helpers')
 
-module.exports = ({ req }) => {
+module.exports = ({ req, errors }) => {
     return layout({content: `
         <div>
             <form method="POST">
                 <input name="email" placeholder="email" type="text">
+                ${getError(errors, 'email')}
                 <input name="password" placeholder="password" type="text">
+                ${getError(errors, 'password')}
                 <input name="passwordConfirmation" placeholder="password confirmation" type="text">
+                ${getError(errors, 'passwordConfirmation')}
                 <button>Sign up</button>
             </form>
         </div>
